b68287ff8450682fb710fbce3067f80e13d00777
Rollingegg <1244797125@qq.com>
2022-02-15 16:08:34 +0800

    [deploy] 添加vercel的代理serverless函数

---
+++ api/proxy.js
@@ -0,0 +1,22 @@
+import { createProxyMiddleware } from 'http-proxy-middleware'
+
+export default (req, res) => {
+  let target = ''
+
+  // 代理目标地址
+  // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
+  if (req.url.startsWith('/backend')) {
+    target = 'http://47.100.63.10:8088'
+  }
+
+  // 创建代理对象并转发请求
+  createProxyMiddleware({
+    target,
+    changeOrigin: true,
+    pathRewrite: {
+      // 通过路径重写，去除请求路径中的 `/backend`
+      // 例如 /backend/user/login 将被转发到 http://47.100.63.10:8088/user/login
+      '^/backend/': '/',
+    },
+  })(req, res)
+}
